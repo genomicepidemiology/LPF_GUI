04861396b8fe6494c6750018057d8640e4828b50
MBHallgren <s153002@student.dtu.dk>
2023-06-07 15:32:44 +0200

    tar3

--- setup_gui.py
+++ setup_gui.py
@@ -9,17 +9,12 @@ setup_gui.py
 import os
 import sys
 
-cmd = 'sudo chmod a+x dist/*'
-os.system(cmd)
-cmd = 'sudo chmod a+x dist/linux-unpacked/*'
-os.system(cmd)
-
 if os.path.exists("/usr/share/applications/lpf.desktop"): #Remove old copy
     os.system("sudo rm /usr/share/applications/lpf.desktop")
 os.system("sudo cp lpf.desktop /usr/share/applications/.")
 
 if os.path.exists("/opt/lpf_app"): #Remove old copy
     os.system("sudo rm -r /opt/lpf_app")
-os.system("sudo mkdir -777 /opt/lpf_app")
+os.system("sudo mkdir -m 777 /opt/lpf_app")
 os.system("sudo cp -r * /opt/lpf_app/.")
 
